a3b6872283f9c5b8f8ec9a1359638cdd953f7342
Aswathi A K <aswathiak.3@gmail.com>
2024-06-27 12:18:00 +0530

    code updated

--- script.js
+++ script.js
@@ -7,18 +7,28 @@ let buttonArray = Array.from(buttons);
 
 buttonArray.forEach((button) => {
   button.addEventListener("click", (e) => {
-    if (e.target.innerHTML === "=") {
-      string = eval(string);
-      input.value = string;
-    } else if (e.target.innerHTML === "AC") {
-      string = "";
-      input.value = string;
-    } else if (e.target.innerHTML === "DEL") {
-      string = string.substring(0, string.length - 1);
-      input.value = string;
-    } else {
-      string += e.target.innerHTML;
-      input.value = string;
+    switch (e.target.innerHTML) {
+      case "AC":
+        string = "";
+        input.value = string;
+        break;
+      case "DEL":
+        string = string.slice(0, -1);
+        input.value = string;
+        break;
+      case "=":
+        try {
+          string = eval(string);
+          input.value = string;
+        } catch (error) {
+          string = "Error";
+          input.value = string;
+        }
+        break;
+      default:
+        string += e.target.innerHTML;
+        input.value = string;
+        break;
     }
   });
 });
